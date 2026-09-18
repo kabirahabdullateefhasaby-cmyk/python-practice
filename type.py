50e5a734f2e7d7921532454f65276514140b289f
ijamiu <imamhassanadanla@gmail.com>
2026-09-18 18:01:53 +0100

    type.py

--- type.py
+++ type.py
@@ -1,7 +1,11 @@
 name = "kabeerah"
+# this is a string
 num = 16
+# this is a int
 hieght = 16.5
+# this is a float
 short = True
+# this is a bool
 print(type(name))
 print(type(num))
 print(type(hieght))

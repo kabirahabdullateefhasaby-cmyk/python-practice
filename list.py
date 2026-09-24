eea2bf8cce4cef890fce869b1c45b6714bcef4ea
ijamiu <imamhassanadanla@gmail.com>
2026-09-24 16:19:34 +0100

    safe_calculator.py

---
+++ list.py
@@ -0,0 +1,13 @@
+tasks = []
+
+print("====== TO DO LIST =====")
+print("1. Add task")
+print("2. View task")
+print("3. Remove task")
+print("4. Exit")
+
+choice = input("choose your option: ")
+print("you selection:", choice)
+
+
+        
---
+++ safe_calculator.py
@@ -0,0 +1,39 @@
+def cafe_calculator(a, operator, b):
+
+    if operator == "+":
+        return a + b
+
+def cafe_calculator(a, operator, b):
+
+    if operator == "+":
+        return a + b
+
+    elif operator == "-":
+        return a - b
+
+    elif operator == "*":
+        return a * b
+
+    elif operator == "/":
+        if b == 0:
+            return "Cannot divide by zero"
+        return round(a / b, 2)
+
+    elif operator == "%":
+        if b == 0:
+            return "Cannot divide by zero"
+        return a % b
+
+    elif operator == "**":
+        return a ** b
+
+    else:
+        return "Invalid operator"
+
+
+print(cafe_calculator(10, "+", 5))
+print(cafe_calculator(10, "-", 3))
+print(cafe_calculator(10, "*", 4))
+print(cafe_calculator(10, "/", 3))
+print(cafe_calculator(10, "%", 3))
+print(cafe_calculator(2, "**", 3))
